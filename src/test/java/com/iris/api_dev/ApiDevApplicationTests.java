3a2490bbeda8b68493e87b72358019c8a48592e9
wirisguedes <wirisguedes@gmail.com>
2024-06-16 13:31:39 -0300

    estrutura inicial projeto

---
+++ src/test/java/com/iris/api_dev/ApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.api_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
