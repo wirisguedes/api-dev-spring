e6b0480095898f811422459e343a4f32791fee0d
wirisguedes <wirisguedes@gmail.com>
2024-06-16 16:03:34 -0300

    implementacao UserRepository

---
+++ src/main/java/com/iris/api_dev/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.iris.api_dev.domain.repository;
+
+import com.iris.api_dev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
