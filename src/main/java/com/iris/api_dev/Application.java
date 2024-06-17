940ab3a24b1782721b72e887d470b64c21934f31
wirisguedes <wirisguedes@gmail.com>
2024-06-16 21:42:20 -0300

    publicacao no railway

--- src/main/java/com/iris/api_dev/Application.java
+++ src/main/java/com/iris/api_dev/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Acessar h2 http://localhost:8080/h2-console
  * http://localhost:8080/swagger-ui/index.html
+ * https://railway.app/
  *
  */
 
